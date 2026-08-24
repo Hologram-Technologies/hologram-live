427fe1425437b08777adbb41bd99b90a9a5ba693
Ari <me@ari.io>
2026-08-24 13:49:13 -0700

    Updated flake

--- flake.nix
+++ flake.nix
@@ -1,18 +1,46 @@
 {
   description = "Hologram Live development environment";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      systems = [ "aarch64-darwin" "x86_64-darwin" "aarch64-linux" "x86_64-linux" ];
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f (import nixpkgs { inherit system; }));
-    in {
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [ rustc cargo rustfmt clippy just nodejs_22 ];
-          RUST_BACKTRACE = "1";
-        };
-      });
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs systems (
+          system:
+          function {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forAllSystems (
+        { pkgs }:
+        {
+          default = pkgs.mkShellNoCC {
+            packages = with pkgs; [
+              cargo
+              rustc
+              rustfmt
+              clippy
+              just
+              watchexec
+              nixfmt
+              nodejs_22
+            ];
+          };
+        }
+      );
+
+      formatter = forAllSystems ({ pkgs }: pkgs.nixfmt);
     };
 }
